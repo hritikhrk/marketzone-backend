254be5f4a51c8c0444054a80a0f9e8708999db6a
Hritik Kumar <hritik.kmr.111@gmail.com>
2023-02-10 14:32:43 +0530

    modify config

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-PORT = process.env.PORT || 5000;
+PORT = process.env.PORT || "5000";
 MONGODB_URL = { URL: process.env.MONGODB_URL, HOST: "CLOUD" } || {
   URL: "mongodb://localhost:27017/marketzone",
   HOST: "LOCAL",
 };
-URL = process.env.HOST_URL || "http://localhost:5000";
+URL = process.env.HOST_URL || "http://localhost:8080";
 JWT_SECRET_KEY =
   process.env.JWT_SECRET_KEY || "60963010-08bd-11ed-bee6-df935beb76ca";
 
